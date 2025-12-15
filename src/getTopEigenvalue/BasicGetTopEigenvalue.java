fb73b13f59b9f45053b17b5f50388ad17b91a360
hidny <mtardibuono@gmail.com>
2025-12-15 15:09:02 -0500

    Found different ways to get top eigenvalues for small N. No closer to the goal though

---
+++ src/getTopEigenvalue/BasicGetTopEigenvalue.java
@@ -0,0 +1,333 @@
+package getTopEigenvalue;
+
+public class BasicGetTopEigenvalue {
+
+	public static void main(String[] args) {
+		
+		int NUM_IT = 6;
+		int PERIOD_DEBUG_PER_LOOP = 1;
+		
+		for(int n=17; n<21; n++) {
+			System.out.println();
+			System.out.println("N = " + n + ":");
+			
+			double vector[] = new double[(int)Math.pow(2, n)];
+			for(int i=0; i<vector.length; i++) {
+				vector[i] = 1.0;
+			}
+			
+			
+			
+			//double prevEignevalue = 0.0;
+			double curEigenvalue = 0.0;
+			for(int i=0; i<NUM_IT; i++) {
+				vector = multCurrentVection(vector, n);
+				curEigenvalue = vector[0];
+				vector = normalizeVector(vector);
+				
+				if(i % PERIOD_DEBUG_PER_LOOP == 0) {
+					
+					System.out.println("Current eigenvalue: " + curEigenvalue);
+				}
+			}
+			
+			System.out.println("Final eigenvalue: " + curEigenvalue);
+			
+			System.out.println("Estimated growth rate: " + Math.pow(curEigenvalue, 1.0/(1.0 * n)));
+			
+		}
+	}
+
+	
+	
+	public static double[] multCurrentVection(double vector[], int n) {
+		
+		double newVector[] = new double[vector.length];
+		
+		for(int i=0; i<vector.length; i++) {
+			
+			boolean bitsI[] = convertToBool(i, n);
+			
+			for(int j=0; j<vector.length; j++) {
+				
+				boolean bitsJ[] = convertToBool(j, n);
+				
+				boolean foundBad2x2Subspace = false;
+				for(int k=0; k<bitsI.length - 1; k++) {
+					if(bitsI[k] != bitsI[k+1] && bitsI[k] == bitsJ[k+1] && bitsI[k+1] == bitsJ[k]) {
+						foundBad2x2Subspace = true;
+						break;
+					}
+				}
+				
+				if( ! foundBad2x2Subspace) {
+					newVector[i] += vector[j];
+				}
+				
+			}
+		}
+		
+		
+		return newVector;
+	}
+	
+	
+	public static double[] normalizeVector(double vector[]) {
+		
+		double newVector[] = new double[vector.length];
+		
+		for(int i=0; i<vector.length; i++) {
+			newVector[i] = vector[i] / vector[0];
+		}
+		
+		return newVector;
+	}
+	
+
+	public static boolean[] convertToBool(int num, int numBinaryDigits) {
+		
+		boolean ret[] = new boolean[numBinaryDigits];
+		
+		int curIndex = numBinaryDigits -1;
+		while(curIndex >= 0) {
+			
+			if(num % 2 == 0) {
+				ret[curIndex] = false;
+			} else {
+				ret[curIndex] = true;
+			}
+			num /= 2;
+			curIndex--;
+			
+		}
+		
+		return ret;
+		
+	}
+
+}
+
+/*
+ * 
+N = 1:
+Current eigenvalue: 2.0
+Current eigenvalue: 2.0
+Current eigenvalue: 2.0
+Current eigenvalue: 2.0
+Current eigenvalue: 2.0
+Current eigenvalue: 2.0
+Current eigenvalue: 2.0
+Current eigenvalue: 2.0
+Current eigenvalue: 2.0
+Current eigenvalue: 2.0
+Final eigenvalue: 2.0
+Estimated growth rate: 2.0
+
+N = 2:
+Current eigenvalue: 4.0
+Current eigenvalue: 3.561797752808989
+Current eigenvalue: 3.561552964178479
+Current eigenvalue: 3.5615528129023803
+Current eigenvalue: 3.561552812808888
+Current eigenvalue: 3.5615528128088303
+Current eigenvalue: 3.5615528128088303
+Current eigenvalue: 3.5615528128088303
+Current eigenvalue: 3.5615528128088303
+Current eigenvalue: 3.5615528128088303
+Final eigenvalue: 3.5615528128088303
+Estimated growth rate: 1.8872076761206835
+
+N = 3:
+Current eigenvalue: 8.0
+Current eigenvalue: 6.41916747337851
+Current eigenvalue: 6.418832759199776
+Current eigenvalue: 6.4188326759906955
+Current eigenvalue: 6.418832675970047
+Current eigenvalue: 6.418832675970042
+Current eigenvalue: 6.418832675970042
+Current eigenvalue: 6.418832675970042
+Current eigenvalue: 6.418832675970042
+Current eigenvalue: 6.418832675970042
+Final eigenvalue: 6.418832675970042
+Estimated growth rate: 1.8584548654699737
+
+N = 4:
+Current eigenvalue: 16.0
+Current eigenvalue: 11.555453097493505
+Current eigenvalue: 11.554619059296712
+Current eigenvalue: 11.554618773794221
+Current eigenvalue: 11.554618773663975
+Current eigenvalue: 11.554618773663908
+Current eigenvalue: 11.554618773663908
+Current eigenvalue: 11.554618773663908
+Current eigenvalue: 11.554618773663908
+Current eigenvalue: 11.554618773663908
+Final eigenvalue: 11.554618773663911
+Estimated growth rate: 1.843694267244297
+
+N = 5:
+Current eigenvalue: 32.0
+Current eigenvalue: 20.80408429758061
+Current eigenvalue: 20.802382227072105
+Current eigenvalue: 20.802381759689716
+Current eigenvalue: 20.80238175952346
+Current eigenvalue: 20.802381759523396
+Current eigenvalue: 20.802381759523403
+Current eigenvalue: 20.802381759523396
+Current eigenvalue: 20.802381759523403
+Current eigenvalue: 20.802381759523396
+Final eigenvalue: 20.802381759523396
+Estimated growth rate: 1.8349431453466731
+
+N = 6:
+Current eigenvalue: 64.0
+Current eigenvalue: 37.45464048856965
+Current eigenvalue: 37.451128752886156
+Current eigenvalue: 37.45112783512576
+Current eigenvalue: 37.451127834791365
+Current eigenvalue: 37.45112783479121
+Current eigenvalue: 37.45112783479121
+Current eigenvalue: 37.45112783479121
+Current eigenvalue: 37.45112783479121
+Current eigenvalue: 37.45112783479121
+Final eigenvalue: 37.45112783479121
+Estimated growth rate: 1.8291282200785635
+
+N = 7:
+Current eigenvalue: 128.0
+Current eigenvalue: 67.43154124099634
+Current eigenvalue: 67.42444832727642
+Current eigenvalue: 67.42444661626912
+Current eigenvalue: 67.42444661569374
+Current eigenvalue: 67.42444661569348
+Current eigenvalue: 67.4244466156935
+Current eigenvalue: 67.4244466156935
+Current eigenvalue: 67.4244466156935
+Current eigenvalue: 67.4244466156935
+Final eigenvalue: 67.4244466156935
+Estimated growth rate: 1.8249863783409301
+
+N = 8:
+Current eigenvalue: 256.0
+Current eigenvalue: 121.40051455081371
+Current eigenvalue: 121.38634451486388
+Current eigenvalue: 121.3863412929786
+Current eigenvalue: 121.38634129194136
+Current eigenvalue: 121.38634129194101
+Current eigenvalue: 121.38634129194098
+Current eigenvalue: 121.38634129194098
+Current eigenvalue: 121.386341291941
+Current eigenvalue: 121.38634129194101
+Final eigenvalue: 121.386341291941
+Estimated growth rate: 1.821886122100499
+
+N = 9:
+Current eigenvalue: 512.0
+Current eigenvalue: 218.56366646872115
+Current eigenvalue: 218.53563887723024
+Current eigenvalue: 218.53563283708382
+Current eigenvalue: 218.53563283523846
+Current eigenvalue: 218.53563283523775
+Current eigenvalue: 218.5356328352377
+Current eigenvalue: 218.53563283523772
+Current eigenvalue: 218.53563283523772
+Current eigenvalue: 218.53563283523772
+Final eigenvalue: 218.53563283523775
+Estimated growth rate: 1.819478456832375
+
+N = 10:
+Current eigenvalue: 1024.0
+Current eigenvalue: 393.4915482239425
+Current eigenvalue: 393.4365565414748
+Current eigenvalue: 393.4365452294762
+Current eigenvalue: 393.43654522618203
+Current eigenvalue: 393.43654522618067
+Current eigenvalue: 393.43654522618067
+Current eigenvalue: 393.43654522618067
+Current eigenvalue: 393.43654522618067
+Current eigenvalue: 393.43654522618067
+Final eigenvalue: 393.43654522618067
+Estimated growth rate: 1.8175546154654834
+
+N = 11:
+Current eigenvalue: 2048.0
+Current eigenvalue: 708.4233209950635
+Current eigenvalue: 708.3161577302218
+Current eigenvalue: 708.3161365792257
+Current eigenvalue: 708.3161365733479
+Current eigenvalue: 708.3161365733455
+Current eigenvalue: 708.316136573346
+Current eigenvalue: 708.3161365733457
+Current eigenvalue: 708.3161365733461
+Current eigenvalue: 708.3161365733456
+Final eigenvalue: 708.3161365733454
+Estimated growth rate: 1.8159820766054082
+
+N = 12:
+Current eigenvalue: 4096.0
+Current eigenvalue: 1275.411388258734
+Current eigenvalue: 1275.2037678129802
+Current eigenvalue: 1275.2037283188286
+Current eigenvalue: 1275.2037283083453
+Current eigenvalue: 1275.2037283083448
+Current eigenvalue: 1275.2037283083444
+Current eigenvalue: 1275.2037283083444
+Current eigenvalue: 1275.2037283083444
+Current eigenvalue: 1275.2037283083444
+Final eigenvalue: 1275.2037283083441
+Estimated growth rate: 1.8146726669147375
+
+N = 13:
+Current eigenvalue: 8192.0
+Current eigenvalue: 2296.189525472479
+Current eigenvalue: 2295.789290844725
+Current eigenvalue: 2295.7892171943954
+Current eigenvalue: 2295.7892171757235
+Current eigenvalue: 2295.789217175704
+Current eigenvalue: 2295.789217175705
+Current eigenvalue: 2295.789217175705
+Current eigenvalue: 2295.789217175705
+Current eigenvalue: 2295.789217175705
+Final eigenvalue: 2295.789217175705
+Estimated growth rate: 1.8135654423366352
+
+N = 14:
+Current eigenvalue: 16384.0
+Current eigenvalue: 4133.9495518444655
+Current eigenvalue: 4133.1813793154715
+Current eigenvalue: 4133.181242133625
+Current eigenvalue: 4133.181242100236
+Current eigenvalue: 4133.181242100231
+Current eigenvalue: 4133.1812421002305
+Current eigenvalue: 4133.1812421002305
+Current eigenvalue: 4133.1812421002305
+Current eigenvalue: 4133.1812421002305
+Final eigenvalue: 4133.1812421002305
+Estimated growth rate: 1.8126169304159052
+
+N = 15:
+Current eigenvalue: 32768.0
+Current eigenvalue: 7442.564608715633
+Current eigenvalue: 7441.095915224615
+Current eigenvalue: 7441.095659993796
+Current eigenvalue: 7441.095659934349
+Current eigenvalue: 7441.095659934319
+Current eigenvalue: 7441.095659934319
+Current eigenvalue: 7441.095659934319
+Current eigenvalue: 7441.095659934319
+Current eigenvalue: 7441.095659934319
+Final eigenvalue: 7441.095659934319
+Estimated growth rate: 1.8117952880347965
+
+N = 16:
+Current eigenvalue: 65536.0
+Current eigenvalue: 13399.236555800777
+Current eigenvalue: 13396.438079555517
+Current eigenvalue: 13396.437605188554
+Current eigenvalue: 13396.437605082583
+Current eigenvalue: 13396.437605082625
+
+N = 17:
+Current eigenvalue: 131072.0
+Current eigenvalue: 20148.741653442383
+*/
---
+++ src/getTopEigenvalue/BruteForceMaxEigenvalue.java
@@ -0,0 +1,226 @@
+package getTopEigenvalue;
+
+import determinantStudy.Permutation;
+import number.IsNumber;
+
+public class BruteForceMaxEigenvalue {
+
+	public static void main(String[] args) {
+		
+		//TODO: n=6 means not using longs for the limits...
+		// Also, it's out of reach.
+		
+		
+		for(int n=1; n<=5; n++) {
+
+			System.out.println();
+			System.out.println();
+			System.out.println("n = " + n);
+			boolean matrix[][] = createMatrix(n);
+			
+			int result[] = new int[((int)Math.pow(2, n)) + 1];
+			
+			String permIndexes[] = new String[matrix.length];
+			for(int j=0; j<matrix.length; j++) {
+				
+				permIndexes[j] = "" + j;
+			}
+			
+			
+			long max = Permutation.getSmallFactorial(permIndexes.length);
+			
+			long itDebug = 0;
+			
+			for(long j=0; j<max; ) {
+				String permToUse[] = Permutation.generatePermutation(permIndexes, j);
+				
+				if(itDebug % 1000000 ==0 ) {
+					for(int k=0; k<permToUse.length; k++) {
+						System.out.print(permToUse[k] + "  ");
+					}
+					System.out.println();
+				}
+				
+				boolean stillGood = true;
+				int numIterationFactorial = permToUse.length - 1;
+				int numMainDiag = 0;
+				
+				for(int k=0; k<permToUse.length; k++) {
+					
+					int colNumberToUse = pint(permToUse[k]);
+					
+					if(matrix[k][colNumberToUse]) {
+						numIterationFactorial--;
+						
+						if(k == colNumberToUse) {
+							numMainDiag++;
+						}
+						
+					} else {
+						stillGood = false;
+						break;
+					}
+				}
+				
+				if(stillGood) {
+					
+					//TODO: we have to figure out the sign!
+					
+					boolean shouldBePositive = true;
+					//TODO: don't initialize every time...
+					boolean used[] = new boolean[permToUse.length];
+					
+					
+					for(int k=0; k<permToUse.length; k++) {
+						
+						int colNumberToUse = pint(permToUse[k]);
+
+						used[colNumberToUse] = true;
+						
+						for(int m=0; m<colNumberToUse; m++) {
+							if(used[m] == false) {
+								shouldBePositive = !shouldBePositive;
+							}
+						}
+					}
+					
+					if(shouldBePositive) {
+						result[result.length - 1 - numMainDiag]++;
+					} else {
+						result[result.length - 1 - numMainDiag]--;
+					}
+				}
+				
+				//Both should get same answer, but the uncommented one should be faster:
+				//j++;
+				j += Permutation.getSmallFactorial(numIterationFactorial);
+				
+				itDebug++;
+			}
+			
+			
+			System.out.println("Result:");
+			
+			for(int k=0; k<result.length; k++) {
+				
+				if(k < result.length - 1 && result[k] != 0) {
+					System.out.print("(" + result[k] + ")" + "(1-y)^" + (result.length-1-k) + " + ");
+				
+				} else if(k == result.length - 1){
+					System.out.print("(" + result[k] + ")");
+				}
+			}
+			System.out.println("= 0");
+			
+		}
+		
+	}
+	
+
+	public static int pint(String s) {
+		if (IsNumber.isNumber(s)) {
+			return Integer.parseInt(s);
+		} else {
+			System.out.println("Error: (" + s + ") is not a number");
+			return -1;
+		}
+	}
+	
+
+	
+	//TODO: what if we order it by the # of times the numbers switch from 0 to 1? Will the picture look clearer?
+		//Right now, it has a fractal shape.
+		public static boolean[][] createMatrix(int numBits) {
+			
+			boolean ret[][] = new boolean[(int)Math.pow(2, numBits)][(int)Math.pow(2, numBits)];
+			
+			for(int i=0; i<Math.pow(2, numBits); i++) {
+				
+				boolean bitsI[] = convertToBool(i, numBits);
+				
+				for(int j=0; j<Math.pow(2, numBits); j++) {
+					boolean bitsJ[] = convertToBool(j, numBits);
+					
+					boolean foundProb = false;
+					for(int k=0; k<bitsI.length - 1; k++) {
+						if(bitsI[k] != bitsI[k+1] && bitsI[k] == bitsJ[k+1] && bitsI[k+1] == bitsJ[k]) {
+							foundProb = true;
+							break;
+						}
+					}
+					if(foundProb) {
+						System.out.print("_");
+						ret[i][j] = false;
+					} else {
+						System.out.print("#");
+						ret[i][j] = true;
+					}
+				}
+				System.out.println();
+			}
+			System.out.println();
+			System.out.println();
+			
+			return ret;
+		}
+		
+		public static boolean[] convertToBool(int num, int numBinaryDigits) {
+			
+			boolean ret[] = new boolean[numBinaryDigits];
+			
+			int curIndex = numBinaryDigits -1;
+			while(curIndex >= 0) {
+				
+				if(num % 2 == 0) {
+					ret[curIndex] = false;
+				} else {
+					ret[curIndex] = true;
+				}
+				num /= 2;
+				curIndex--;
+				
+			}
+			
+			return ret;
+			
+		}
+
+}
+
+/*
+ * 
+
+n = 1
+##
+##
+
+
+Result:
+1(1-y)^2 + 0(1-y)^1 + 1
+
+
+n = 2
+####
+##_#
+#_##
+####
+
+
+Result:
+1(1-y)^4 + 0(1-y)^3 + 5(1-y)^2 + 4(1-y)^1 + 4
+
+
+n = 3
+########
+##_###_#
+#_##__##
+####__##
+##__####
+##__##_#
+#_###_##
+########
+
+
+Result:
+1(1-y)^8 + 0(1-y)^7 + 21(1-y)^6 + 48(1-y)^5 + 236(1-y)^4 + 604(1-y)^3 + 1304(1-y)^2 + 1792(1-y)^1 + 1208
+*/
