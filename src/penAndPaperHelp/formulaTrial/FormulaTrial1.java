1a309a7883bf0a6c718daf1636efe95686109371
hidny <mtardibuono@gmail.com>
2025-12-23 00:00:51 -0500

    Attempted to apply the formulas I came up with. I doubt that they work

--- src/penAndPaperHelp/formulaTrial/FormulaTrial1.java
+++ src/penAndPaperHelp/formulaTrial/FormulaTrial1.java
@@ -8,10 +8,108 @@ public class FormulaTrial1 {
 	
 	//TODO: solve via dynamic programming.
 	// My feeling is that the formula is wrong somehow and I'll be nonsense answers.
-	
+
 	
 	public static void main(String[] args) {
-		setupPerm();
+		//setupPerm();
+		
+		
+		//[1]
+		//[1, 2]
+		//[1, 2, 3]
+		//[1, 2, 3, 4]
+		
+		getEquation(30, 20);
+		
+		
+	}
+
+	public static double[] getEquation(int numCoef, int n) {
+
+		double firstNCoef[][] = new double[numCoef][numCoef];
+		
+		for(int j=0; j<firstNCoef[0].length; j++) {
+			firstNCoef[0][j] = 1.0;
+		}
+		
+		for(int i=1; i<firstNCoef.length; i++) {
+			for(int c=0; c<firstNCoef[0].length; c++) {
+				
+				firstNCoef[i][c] = 0.0;
+				
+				if(c > 0) {
+					firstNCoef[i][c] += firstNCoef[i][c-1];
+				}
+
+				if(c >= 2) {
+					//TODO: You could make this faster:
+					for(int b=1; c*b<=i; b++) {
+						
+						if(c == 2) {
+							firstNCoef[i][c] += firstNCoef[i-c*b][c-1] * getEstimateCoefficientsForB_2CyclesForY(n, K, P, b);
+						} else {
+							firstNCoef[i][c] += firstNCoef[i-c*b][c-1] * getEstimateCoefficientsForD_CCyclesForY(n, c, b);
+							
+						}
+					}
+				}
+				
+				System.out.print(firstNCoef[i][c] + ", ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		System.out.println();
+
+		double ret[] = new double[firstNCoef.length];
+		
+		for(int i=0; i<ret.length; i++) {
+			ret[i] = firstNCoef[i][firstNCoef[0].length - 1];
+			
+			System.out.print(ret[i] + ", ");
+		}
+		
+
+		for(int i=0; i<ret.length; i++) {
+			ret[i] = firstNCoef[i][firstNCoef[0].length - 1];
+			
+			System.out.print(ret[i] + ", ");
+		}
+
+		System.out.println();
+		System.out.println();
+
+		for(int i=0; i<ret.length; i++) {
+			
+			System.out.print(ret[i] + "*y^" + (numCoef - i - 1));
+			
+			if(i<ret.length - 1) {
+				System.out.print(" + ");
+			}
+		}
+		System.out.print(" = 0 ");
+		System.out.println();
+		
+		return ret;
+	}
+	// wolfram: getEquation(10, 20);
+	// y = 0.197496
+	// x = M*(p^n*k) * y
+	// x  = (n/p)^n *k * y = ( 2.0/(sqrt(17) -3.0))^20*(0.5 + 2.0/sqrt(17))* 0.197496
+	//  =20008.5
+	// g approx x^(1/20)
+	// g = 1.640819
+	// Dph!
+	
+	//TODO: solve polynomial with binary search
+	//TODO: automate the g calc test
+	// TODO: Does the assumption that cycles >=3 are random with no bias even hold?
+	// For example P(s1 connects s3) == P( s1 connects s3 given s1 connects to s2 and s2 connects to s3) ???
+	// I assumed it did, but that's starting to seem foolish.
+	
+	
+	
+	public static void display() {
 		
 		double sum = 0.0;
 		for(int b=0; b<100; b++) {
@@ -135,15 +233,15 @@ public class FormulaTrial1 {
 	}
 	
 	
-	public static double permB[] = new double[100];
+	public static double permB[] = setupPerm();
 	
-	public static void setupPerm() {
-		permB = new double[100];
-		permB[0] = 1;
-		for(int i=1; i<permB.length; i++) {
-			permB[i] = i * permB[i-1];
-			System.out.println(i + "! : " +  permB[i]);
+	public static double[] setupPerm() {
+		double ret[] = new double[100];
+		ret[0] = 1;
+		for(int i=1; i<ret.length; i++) {
+			ret[i] = i * ret[i-1];
+			System.out.println(i + "! : " +  ret[i]);
 		}
-		
+		return ret;
 	}
 }
