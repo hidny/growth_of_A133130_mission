a61151aa0075a0434bc146aef8408506d65ce58e
hidny <mtardibuono@gmail.com>
2026-05-28 23:01:58 -0400

    Made a gui to represent what the matrix looks like

--- src/getTopTriangleEigenvector/DrawOnGui.java
+++ src/getTopTriangleEigenvector/DrawOnGui.java
@@ -25,13 +25,18 @@ public class DrawOnGui implements ActionListener {
 	private JButton button2;
 	private int count = 0;
 	
+	//TODO: make this shiftable:
+	private int N = 5;
+	
+	public static final int DIM = 512;
+	
 	public DrawOnGui() {
 		frame = new JFrame();
 		frame.setSize(600, 800);
 		
-		button = new JButton("Click me");
+		button = new JButton("Increase N");
 		button.addActionListener(this);
-		button2 = new JButton("Click me 2");
+		button2 = new JButton("Decrease N");
 		button2.addActionListener(this);
 		
 		label = new JLabel("Number of clicks: " + count);
@@ -60,15 +65,123 @@ public class DrawOnGui implements ActionListener {
 		
 	}
 	
+	public static boolean[][] getTableTubeSquareLattice(int numBits) { 
+		
+		int length = (int)Math.pow(2, numBits);
+
+		int RELEVANT_TILES = (int)Math.pow(2, numBits) - 1;
+		int RELEVANT_TILES_NOT_LEFTMOST = (int)Math.pow(2, numBits - 1) - 1;
+		
+		boolean ret[][] = new boolean[length][length];
+		
+		for(int i=0; i<ret.length; i++) {
+
+			int wrappedBottom =  (i << numBits) + i;
+			
+			int rhsToCheck = ((wrappedBottom >> 1) ^ wrappedBottom) & RELEVANT_TILES;
+			
+			for(int j=0; j<Math.pow(2, numBits); ) {
+				
+				int wrappedTop = ((j << numBits) + j);
+				
+				int tmpCheckProb = (wrappedBottom ^ wrappedTop) & (~(wrappedBottom ^ (wrappedTop >> 1))) & rhsToCheck;
+				
+				if( tmpCheckProb != 0) {
+					//Collision:
+					
+					/* From random thread poster called VArtem: (codeforces)
+					 * There is Integer.highestOneBit(int i) method in Java that returns int with leftmost bit set in x. It is implemented as follows:
+
+					public static int highestOneBit(int i) {
+					    i |= (i >>  1);
+					    i |= (i >>  2);
+					    i |= (i >>  4);
+					    i |= (i >>  8);
+					    i |= (i >> 16);
+					    return i - (i >>> 1);
+					}
+					*/
+					//find the index of the leftmost bit:
+					int getHighestBit = tmpCheckProb;
+					//Copied algo that's O(log(size)) instead of just O(size):
+					getHighestBit |= (getHighestBit >>  1);
+					getHighestBit |= (getHighestBit >>  2);
+					getHighestBit |= (getHighestBit >>  4);
+					getHighestBit |= (getHighestBit >>  8);
+					getHighestBit |= (getHighestBit >> 16);
+					
+					
+					int answer = getHighestBit - (getHighestBit >>> 1);
+					
+					//The two variables below are made to get rid of one if condition about how to handle case
+					//where the leftmost bit interferes with rightmost tile.
+					int answerisLeftMostTile = (answer >> (numBits-1)) & 1;
+					int answerNotLeftMost = answer & RELEVANT_TILES_NOT_LEFTMOST;
+					
+					
+				    j += answerisLeftMostTile + answerNotLeftMost;
+
+				} else {
+
+					//No Collision:
+					ret[i][j] = true;
+					j++;
+				}
+				
+				
+				
+			}
+			
+		}
+		
+		return ret;
+	}
+	
 	private class GPanel extends JPanel {
 		
 		@Override
 		public void paintComponent(Graphics g) {
+
 			super.paintComponent(g);
+			
+			int startX= 10;
+			int startY = 10;
+			
+			int numDivs = (int) Math.pow(2, N);
+			int gap = DIM / numDivs;
+			System.out.println("Gap: " + gap);
+			System.out.println("numDivs: " + numDivs);
+			
+			int DIV_BETWEEN_SQUARES = 1;
+			
+			if(N >= 8) {
+				DIV_BETWEEN_SQUARES = 0;
+			}
+			
+			boolean ret[][] = getTableTubeSquareLattice(N);
+			
+			for(int i=0; i<numDivs; i++) {
+				for(int j=0; j<numDivs; j++) {
+					/*if((i + j) % 2 == 0) {
+						g.setColor(Color.BLUE);
+					} else {
+						g.setColor(Color.YELLOW);
+						
+					}*/
+					if(ret[i][j]) {
+						g.setColor(Color.BLUE);
+					} else {
+						g.setColor(Color.YELLOW);
+					}
+					
+					g.fillRect(startY + j*gap, startX + i*gap, gap - DIV_BETWEEN_SQUARES, gap - DIV_BETWEEN_SQUARES);
+				}
+			}
+			
 			//g.drawRect(10,10, 190, 190);
-			g.drawRect(10,10, 522, 522);
+			g.drawRect(startX, startY, DIM -1, DIM -1);
 			g.setColor(Color.BLUE);
-			g.fillOval(50, 50, 100, 100);
+			//g.fillOval(50, 50, 100, 100);
 		}
 	}
 	
@@ -81,9 +194,31 @@ public class DrawOnGui implements ActionListener {
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent e) {
+	public void actionPerformed(ActionEvent event) {
 		count++;
 		label.setText("Number of clicks: " + count);
+		
+		String buttonText = event.getActionCommand();
+		
+		System.out.println("Button Text: " + buttonText);
+		if(buttonText.equals("Increase N")) {
+			N++;
+			
+			if(N >= 10) {
+				N = 9;
+			}
+			
+		} else if(buttonText.equals("Decrease N")) {
+			N--;
+			
+			if(N <= 0) {
+				N = 1;
+			}
+			
+		}
+		System.out.println("N: " + N);
+		panelDrawing.repaint();
+		
 	}
 
 }
