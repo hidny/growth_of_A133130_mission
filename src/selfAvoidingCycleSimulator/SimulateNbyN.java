488881da78b9cc2bd32818a6805cb0354d2449d4
hidny <mtardibuono@gmail.com>
2026-06-06 23:58:18 -0400

    Added way of getting random sample solution I think

--- src/selfAvoidingCycleSimulator/SimulateNbyN.java
+++ src/selfAvoidingCycleSimulator/SimulateNbyN.java
@@ -1,6 +1,9 @@
 package selfAvoidingCycleSimulator;
 
 import java.math.BigInteger;
+import java.security.SecureRandom;
+import java.util.Hashtable;
+import java.util.Set;
 
 public class SimulateNbyN {
 
@@ -8,14 +11,82 @@ public class SimulateNbyN {
 	//June 6th, 2026
 	//I feel like I did this before, and I probably did it more efficiently too.
 	//AHA: I did it in misselaneous.growthRateMultLoops.GetNumWaysToGridWithMultLoops.java
+
+	public static Hashtable<String, Integer> debugCounter = new Hashtable<String, Integer>();
 	
 	public static void main(String[] args) {
 
-		solve(20);
+		System.out.println("Drawing random solutions:");
+		//for(int i=0; i<1000000; i++) {
+			//drawRandom(4);
+		//}
+	
+		for(int i=0; i<10000000; i++) {
+			drawRandom(2);
+		}
+		
+		
+		Set<String> keySet = debugCounter.keySet();
+		
+		int numKeys = 0;
+		// Loop through the keys
+        for (String key : keySet) {
+            System.out.println("Key:\n" + key);
+            System.out.println("Values: " + debugCounter.get(key));
+            numKeys++;
+        }
+        
+        System.out.println("Number of keys: " + numKeys);
+        
+        
+	}
+	
+	
+	public static void drawRandom(int length) {
+		
+		String ret = "";
+		int lastLayerBinary = -1;
+		for(int rowIndex=length-1; rowIndex>=0; rowIndex--) {
+			
+			lastLayerBinary = solve(length, rowIndex, lastLayerBinary);
+			
+			//Just print the last row 1st. It shouldn't really matter:
+			ret = getLine(lastLayerBinary, length) + ret;
+			
+		}
+		//System.out.println(ret);
+		
+		if(debugCounter.containsKey(ret)) {
+			int numInc = debugCounter.get(ret) + 1;
+			debugCounter.remove(ret);
+			debugCounter.put(ret, numInc);
+			
+		} else {
+			debugCounter.put(ret, 1);
+		}
+		
+	}
+	
+	public static String getLine(int rowBinary, int numBits) {
+		
+		int cur = rowBinary;
+		String ret = "";
+		for(int i=0; i<numBits; i++) {
+			
+			if(cur % 2 == 0) {
+				ret += "#";
+			} else {
+				ret += "_";
+			}
+			cur /= 2;
+		}
+		return ret + "\n";
+		
 	}
 
+	public static SecureRandom randomSource = new SecureRandom();
 	
-	public static void solve(int n) {
+	public static int solve(int n, int rowIndex, int nextLayer) {
 		
 		BigInteger curList[] = new BigInteger[(int)Math.pow(2, n + 1)];
 		
@@ -35,7 +106,7 @@ public class SimulateNbyN {
 		
 		BigInteger prevList[];
 		
-		for(int i=1; i<n; i++) {
+		for(int i=0; i<rowIndex; i++) {
 			
 			for(int j=0; j<n; j++) {
 				prevList = curList;
@@ -104,13 +175,107 @@ public class SimulateNbyN {
 			}
 		}
 		
-		
+
 		BigInteger sum = BigInteger.ZERO;
-		for(int i=0; i<curList.length; i++) {
-			sum = sum.add(curList[i]);
+		if(rowIndex + 1 >= n) {
+			for(int i=0; i<curList.length; i++) {
+				sum = sum.add(curList[i]);
+			}
+			
+			//System.out.println("Sum for n = " + n + " and row index " + rowIndex + ": " + sum);
+		
+		} else {
+			
+			int nextLayerInv = ~nextLayer;
+			
+			int maskToCheck = (int)Math.pow(2, n-1) - 1;
+			
+			for(int i=0; i<curList.length; i++) {
+				
+
+				int prevLayerCheck = (i ^ (i >> 1)) & maskToCheck;
+				
+				int nextLayerCheck = (nextLayerInv ^ (nextLayerInv >> 1)) & maskToCheck;
+				
+				if((prevLayerCheck & nextLayerCheck & (i ^ nextLayer)) == 0) {
+					sum = sum.add(curList[i]);
+				}
+			}
+			
+			//System.out.println("Sum for n = " + n + " and row index " + rowIndex + ": " + sum);
+		
+			
+			
 		}
 		
-		System.out.println("Sum for n = " + n + ": " + sum);
+		int numBitsRandom = sum.bitLength() + 15;
 		
+		BigInteger randomNumber;
+		
+		BigInteger twoPowerN = BigInteger.ONE.shiftLeft(numBitsRandom);
+		
+		BigInteger rejectMin = twoPowerN.divideAndRemainder(sum)[1];
+		
+		BigInteger indexAnswer = null;
+		
+		do {
+		    randomNumber = new BigInteger(numBitsRandom, randomSource);
+		    
+		    indexAnswer = randomNumber.divideAndRemainder(sum)[1];
+		    
+		} while (randomNumber.compareTo(rejectMin) < 0);
+		
+		//System.out.println("Random index:\n" + indexAnswer);
+		//System.out.println("Sum:\n" + sum);
+		
+		
+		
+		BigInteger curSum = BigInteger.ZERO;
+		int ret = -1;
+		
+		if(rowIndex + 1 >= n) {
+			for(int i=0; i<curList.length; i++) {
+				curSum = curSum.add(curList[i]);
+				
+				if(curSum.compareTo(indexAnswer) > 0) {
+					ret = i;
+					break;
+				}
+			}
+			
+		
+		} else {
+			
+			int nextLayerInv = ~nextLayer;
+			
+			int maskToCheck = (int)Math.pow(2, n-1) - 1;
+			
+			for(int i=0; i<curList.length; i++) {
+
+				int prevLayerCheck = (i ^ (i >> 1)) & maskToCheck;
+				
+				int nextLayerCheck = (nextLayerInv ^ (nextLayerInv >> 1)) & maskToCheck;
+				
+				if((prevLayerCheck & nextLayerCheck & (i ^ nextLayer)) == 0) {
+					curSum = curSum.add(curList[i]);
+					
+					if(curSum.compareTo(indexAnswer) > 0) {
+						ret = i;
+						break;
+					}
+					
+				}
+			}
+			
+			
+		}
+		
+		if(ret == -1) {
+			System.out.println("Doh! Ret == -1 ");
+			System.exit(1);
+		}
+		
+		//TODO:
+		return ret;
 	}
 }
